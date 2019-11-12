d55baf645656f58a08b618e954c25b49e464eafb
hbbr <46181963+hbbr0312@users.noreply.github.com>
2019-11-12 19:37:29 +0900

    download images from url

---
+++ downloader.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import requests
+import argparse
+from tqdm import tqdm
+
+
+def download(url, file_name):
+    with open(file_name, "wb") as file:   # open in binary mode
+        response = requests.get(url)      # get request
+        file.write(response.content)
+
+def main(args):
+    
+    df = pd.read_csv(args.csv)
+    
+    for index, row in tqdm(df.iterrows()):
+        if index>args.num:
+            break
+        url = row[0]
+        filename = '{}/{}.jpg'.format(args.dir,url.split('/')[-2])
+        #print(filename)
+        download(url, filename)
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('—csv', help='csv filename', default = 'face_1-95.csv')
+    parser.add_argument('—num', help='number of images to download',type = int, default = 10)
+    parser.add_argument('—dir', help='images download path', default = 'images')
+
+    args = parser.parse_args()
+    main(args)
